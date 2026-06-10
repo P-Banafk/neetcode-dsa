97012172c67efd1f67b7c70977f20bafeb0f834a
P-Banafk <praj.ban05@gmail.com>
2026-06-10 09:55:32 +0530

    Add: maximum-frequency-stack - submission-0

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-0.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class FreqStack:
+
+    def __init__(self):
+        self.freq = {}                   
+        self.group = defaultdict(list)    
+        self.maxFreq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] = self.freq.get(val, 0) + 1
+
+        f = self.freq[val]
+        self.maxFreq = max(self.maxFreq, f)
+
+        self.group[f].append(val)
+
+    def pop(self) -> int:
+        val = self.group[self.maxFreq].pop()
+
+        self.freq[val] -= 1
+
+        if not self.group[self.maxFreq]:
+            self.maxFreq -= 1
+
+        return val
